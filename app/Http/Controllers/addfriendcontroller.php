151eb04e3bb93a45d202f6fa529a45a85377c4d9
Umar <umari4042@gamil.com>
2021-11-14 21:53:00 +0500

    final

--- app/Http/Controllers/addfriendcontroller.php
+++ app/Http/Controllers/addfriendcontroller.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\AddFriendValidation;
+use App\Http\Requests\RemoveFriendValidation;
 use App\Models\Friend;
 
 class AddFriendController extends Controller
 {
     /**
      * add_friend function
-     * this function add one user ti another user if the user exit 
+     * this function add one user to another user if the user exit 
      */
     function add_friend(AddFriendValidation $req)
     {
@@ -47,6 +48,34 @@ class AddFriendController extends Controller
         }
         else{
             return response(['Message'=>'Please login Account']);
+        }
+    }
+    function removeFriend(RemoveFriendValidation $req)
+    {
+        $req->validated();
+        $data = DB::table('users')->where('remember_token', $req->token)->get();    //database querie
+        $check=count($data);    
+        if($check>0)    //if condition check user is login in or not
+        {
+            $id1=$data[0]->u_id;
+            $data1 = DB::table('users')->where('email', $req->email)->get();    //database querie
+            $check1=count($data1);
+            if($check1>0) //if condition check 2nd user exit or not
+            {   
+                $id2=$data1[0]->u_id;
+                $data3=DB::table('friends')->where(['user1'=> $id1 , 'user2' =>$id2])->delete();
+                //$conform=count($data3);
+                if($data3)
+                {
+                    return response(['Message'=>'Friend Remove Conform']);
+                }
+                else{
+                    return response(['Message'=>'You are not Friend This user ']);
+                }
+            }
+            else{
+                return response(['Message'=>'Account Not exists']);
+            }
         }
     }
 }
